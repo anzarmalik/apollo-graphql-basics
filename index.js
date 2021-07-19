bb5e909cc7860beb3cbef1dcec4967ab5d311423
anzarmalik <anzarmalikcse@gmail.com>
2021-07-19 18:57:45 +0530

    added Query examples

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const {ApolloServer,gql} = require('apollo-server');
 
 const typeDefs = gql`
- 
+
+  type User {
+      id:ID
+      name : String
+  }
+
    type Query {
-       message : String!  
+       message : String!,
+       users : [User!]!,
+       user(id: String) :[User!]!,
    }
-`
+
+  `;
+
 
 const resolvers = {
     Query:{
+
         message:()=>{
              return " this is message "
-        }
+        },
+        users:()=>{
+            return [{id:'1',name:'anzar'},{id:'2',name:'syed'}]
+       },
+       user:(parent ,{id})=>{
+       console.log("🚀 ~ file: index.js ~ line 29 ~ id 00000", id)
+           return [{id:'1',name:'anzar'},{id:'2',name:'syed'}].filter((user)=>{
+                 return user.id === id ;
+           })
+      }
+
     }
 }
 
@@ -22,9 +42,9 @@ const server  = new ApolloServer({
 
 
 server.listen({
-    port:3000
+    port:4000
 }).then((res)=>{
-    console.log("Server listening"+res.url)
+    console.log("Server listening     "+res.url)
 }).catch((err)=>{
     console.log(err)
 })
