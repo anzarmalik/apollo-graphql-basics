dd31257fe148be148cf9589835c2d2397b6115ef
anzarmalik <anzarmalikcse@gmail.com>
2021-07-19 20:23:23 +0530

    added mutations and tested it

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const {ApolloServer,gql} = require('apollo-server');
 
+const userArray = [{id:'0',name:'anzar'},{id:'1',name:'syed'}];
+
 const typeDefs = gql`
 
-  type User {
+   type User {
       id:ID
       name : String
-  }
+   }
 
    type Query {
        message : String!,
@@ -13,25 +15,42 @@ const typeDefs = gql`
        user(id: String) :[User!]!,
    }
 
+  type Mutation {
+    createUser(name: String!): User
+   }
+
   `;
 
 
 const resolvers = {
+    
     Query:{
-
         message:()=>{
              return " this is message "
         },
         users:()=>{
-            return [{id:'1',name:'anzar'},{id:'2',name:'syed'}]
+            return userArray
        },
        user:(parent ,{id})=>{
        console.log("🚀 ~ file: index.js ~ line 29 ~ id 00000", id)
-           return [{id:'1',name:'anzar'},{id:'2',name:'syed'}].filter((user)=>{
+           return userArray.filter((user)=>{
                  return user.id === id ;
            })
       }
 
+    },
+
+    Mutation:{
+        createUser :(parent,args)=>{
+              const {name} = args ;
+              const id = userArray.length;
+              const userforArray = {
+                  id,
+                  name
+              };
+              userArray.push(userforArray);
+              return userforArray
+        }
     }
 }
 
